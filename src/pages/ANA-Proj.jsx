0a7cae0039fdf701344af448447975e4c3350b78
roidfree <calling_dameer@hotmail.com>
2025-05-19 08:50:21 +0100

    Stylised timer

--- src/pages/ANA-Proj.jsx
+++ src/pages/ANA-Proj.jsx
@@ -5,7 +5,9 @@ import './ANA-Proj.css'; // Import the CSS file
 const ANAProj = () => (
   <div className="ana-proj">
     <main>
+      {/* <h1>ANA - Advanced Neural Adhesive</h1> */}
       <Timer targetDate={new Date('2025-05-21T00:00:00')} />
+      {/* <h2> Coming soon... </h2> */}
     </main>
   </div>
 );
