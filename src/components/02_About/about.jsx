9348ae1ab1873824c2dfb28323eea33cd7cd4ac5
roidfree <calling_dameer@hotmail.com>
2025-06-06 17:46:56 +0100

    fix: public ref and .gitignore for .netlify (need to track)

--- src/components/02_About/about.jsx
+++ src/components/02_About/about.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { FaBrain, FaVideo, FaLightbulb } from 'react-icons/fa';
-import faviconImage from '/favicon.png';
+import faviconImage from '/public/favicon.png';
 import './about.css';
 import teachingImage from '@images/teaching.webp';
 
